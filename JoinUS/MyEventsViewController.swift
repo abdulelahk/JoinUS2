32969c40d163051a1a1089bf5119bf82109b4f85
MoathAhmad <MoathAhmad@moaths-macbook-pro.local>
2017-02-18 01:42:21 +0300

    My Events UI

--- JoinUS/MyEventsViewController.swift
+++ JoinUS/MyEventsViewController.swift
@@ -48,7 +48,17 @@ class MyEventsViewController: UIViewController, UITableViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
+    private func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
 }
 
